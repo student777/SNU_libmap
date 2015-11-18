ff2c8c9ea1d6a1216aa31c5d70eeeaa49a9f9f00
student777 <darkdakku@gmail.com>
2015-11-18 20:32:55 +0900

    번호검색결과 없을때 메시지 추가

--- shelf_map/views.py
+++ shelf_map/views.py
@@ -8,6 +8,8 @@ def index(request):
         major_id = request.GET.get('major_id')
         # minor_id = request.GET.get('minor_id')
         result = Shelf.objects.filter(major_id__lte=major_id).order_by('major_id').last()
+        if result is None:
+            result = '검색 결과가 없습니다(DB 더 채워야함)'
         return render(request, 'index.html', {'result': result})
     elif request.GET.get('title'):
         title = request.GET.get('title')
