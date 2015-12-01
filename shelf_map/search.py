8a1e240cc5a7deb3ab383df31178de6a5c01ede8
student777 <darkdakku@gmail.com>
2015-12-01 10:22:46 +0000

    숫자 검색 에러처리

--- shelf_map/search.py
+++ shelf_map/search.py
@@ -1,23 +1,26 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import re
-from shelf_map.models import *
-
+from shelf_map.models import Shelf_1, Shelf_2, Shelf_3, Shelf_4, Shelf_5, Shelf_6, Shelf_7
+from django.http import Http404
 
 def search_shelf(major_id):
-    if '0' < major_id <= '099':
+    if 0 < major_id < 100:
         return Shelf_1
-    elif '100' < major_id <= '303.4':   # todo
+    elif 100 < major_id <= 303.4:   # todo
         return Shelf_2
-    elif '303.4' < major_id < '400':
+    elif 303.4 < major_id < 400:
         return Shelf_3
-    elif '400' <= major_id < '616.855000':
+    elif 400 <= major_id < 616.855000:
         return Shelf_4
-    elif '616.858000' <= major_id < '700':
+    elif 616.858000 <= major_id < 700:
         return Shelf_5
-    elif '700' <= major_id < '899':
+    elif 700 <= major_id < 900:
         return Shelf_6
-    return 0
+    elif 900 <= major_id < 1000: 
+        raise Http404('7열람실 자료는 준비중입니다')
+    else:
+        raise Http404('0~999 범위의 숫자를 입력해 주세요.')
 
 
 def search_title(text):
@@ -41,12 +44,13 @@ def search_title(text):
             location = sss + '은(는) 올바른 입력 값이 아닙니다(TODO)\n'
         else:
             major_id = ss.group()
+            Shelf = search_shelf(float(major_id))
             shelf_list = Shelf.objects.filter(major_id__lte=major_id).order_by('major_id')
             if shelf_list.exists():
                 shelf = shelf_list.last()
                 location = major_id + '/' + shelf.col + shelf.row
             else:
-                location = major_id + ' 근처의 도서번호가 존재하지 않습니다\n'
+                location = major_id + ' 근처의 도서번호가 존재하지 않습니다(7열람실)\n'
         name_msg_list.append((name, location))
         i += 1
     return name_msg_list
--- shelf_map/views.py
+++ shelf_map/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from shelf_map.search import search_title, search_shelf
-from shelf_map.models import Shelf_1
+from django.http import Http404
 
 
 def index(request):
     if request.GET.get('major_id'):
         major_id = request.GET.get('major_id')
-        Shelf = search_shelf(major_id)
+        try:
+            Shelf = search_shelf(float(major_id))
+        except ValueError:
+            raise Http404('大도서, A나 K로 시작하는 도서 검색은 준비중입니다.')
         if Shelf.objects.filter(major_id=major_id).count() > 1:
             minor_id = request.GET.get('minor_id')
             result = Shelf.objects.filter(major_id=major_id, minor_id__lte=minor_id).order_by('minor_id').last()
@@ -21,6 +24,3 @@ def index(request):
         return render(request, 'index.html')
 
 
-def list(request):
-    list = Shelf_1.objects.all()
-    return render(request, 'list.html', {'list': list})
