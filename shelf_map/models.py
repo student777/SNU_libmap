b9e0187fdd362ae343c184c49660be87f38c9438
memgoon <mem0517@naver.com>
2015-12-01 20:39:41 +0900

    Update models.py
    room_num으로 자료실 번호를 관리하기 위해 수정하였고
    major_id의 처음이 문자일 경우에 대한 예외 처리 및 플래그를 만들었습니다

--- shelf_map/models.py
+++ shelf_map/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+import re
 
-
-class Shelf_1(models.Model):
+class Shelf(models.Model):
+    room_num = models.DecimalField(max_digits = 2, decimal_places=2)
     col = models.CharField(max_length=4)
     row = models.CharField(max_length=4)
     major_id = models.DecimalField(max_digits=11, decimal_places=7)
     minor_id = models.CharField(max_length=20)
+    
+    # ex) "대810" : major_leadingChr - True, major_bigBook - True
+    # ex) "K809" : major_leadingChr - True, major_bigBook - False
+    major_leadingChr = models.BooleanFiled()
+    major_bigBook = models.BooleanFiled()
 
     class Meta:
         ordering = ('major_id', 'minor_id', )
@@ -13,97 +19,22 @@ class Shelf_1(models.Model):
     def __str__(self):
         return str(self.major_id) + ' ' + self.minor_id
 
+def create_shelf(room_num, col, row, major_id, minor_id):
+    # without encapsulation...
+    a = Shelf()
+    
+    # Have leading character?
+    if bool(re.match("[0-9]", major_id[0])):
+        a.major_leadingChr = False
+        a.major_bigBook = False
+        # Can assign major_id freely
+        a.room_num, a.col, a.row, a.major_id, a.minor_id = room_num, col, row, major_id, minor_id
+        
+    else:
+        a.major_leadingChr = True
+        # If it starts with "대", then it is big book.
+        a.majorbigBook = True if major_id[0] == "대" else False
+        # Cannot assign major_id freely, remove first character
+        a.room_num, a.col, a.row, a.major_id, a.minor_id = room_num, col, row, major_id[1:], minor_id
 
-class Shelf_2(models.Model):
-    col = models.CharField(max_length=4)
-    row = models.CharField(max_length=4)
-    major_id = models.DecimalField(max_digits=11, decimal_places=7)
-    minor_id = models.CharField(max_length=20)
-
-    class Meta:
-        ordering = ('major_id', 'minor_id', )
-
-    def __str__(self):
-        return str(self.major_id) + ' ' + self.minor_id
-
-
-class Shelf_3(models.Model):
-    col = models.CharField(max_length=4)
-    row = models.CharField(max_length=4)
-    major_id = models.DecimalField(max_digits=11, decimal_places=7)
-    minor_id = models.CharField(max_length=20)
-
-    class Meta:
-        ordering = ('major_id', 'minor_id', )
-
-    def __str__(self):
-        return str(self.major_id) + ' ' + self.minor_id
-
-
-class Shelf_4(models.Model):
-    col = models.CharField(max_length=4)
-    row = models.CharField(max_length=4)
-    major_id = models.DecimalField(max_digits=11, decimal_places=7)
-    minor_id = models.CharField(max_length=20)
-
-    class Meta:
-        ordering = ('major_id', 'minor_id', )
-
-    def __str__(self):
-        return str(self.major_id) + ' ' + self.minor_id
-
-
-class Shelf_5(models.Model):
-    col = models.CharField(max_length=4)
-    row = models.CharField(max_length=4)
-    major_id = models.DecimalField(max_digits=11, decimal_places=7)
-    minor_id = models.CharField(max_length=20)
-
-    class Meta:
-        ordering = ('major_id', 'minor_id', )
-
-    def __str__(self):
-        return str(self.major_id) + ' ' + self.minor_id
-
-
-class Shelf_6(models.Model):
-    col = models.CharField(max_length=4)
-    row = models.CharField(max_length=4)
-    major_id = models.DecimalField(max_digits=11, decimal_places=7)
-    minor_id = models.CharField(max_length=20)
-
-    class Meta:
-        ordering = ('major_id', 'minor_id', )
-
-    def __str__(self):
-        return str(self.major_id) + ' ' + self.minor_id
-
-
-def create_shelf_1(col, row, major_id, minor_id):
-    a = Shelf_1(col=col, row=row, major_id=major_id, minor_id=minor_id)
-    a.save()
-
-
-def create_shelf_2(col, row, major_id, minor_id):
-    a = Shelf_2(col=col, row=row, major_id=major_id, minor_id=minor_id)
-    a.save()
-
-
-def create_shelf_3(col, row, major_id, minor_id):
-    a = Shelf_3(col=col, row=row, major_id=major_id, minor_id=minor_id)
-    a.save()
-
-
-def create_shelf_4(col, row, major_id, minor_id):
-    a = Shelf_4(col=col, row=row, major_id=major_id, minor_id=minor_id)
-    a.save()
-
-
-def create_shelf_5(col, row, major_id, minor_id):
-    a = Shelf_5(col=col, row=row, major_id=major_id, minor_id=minor_id)
-    a.save()
-
-
-def create_shelf_6(col, row, major_id, minor_id):
-    a = Shelf_6(col=col, row=row, major_id=major_id, minor_id=minor_id)
     a.save()
