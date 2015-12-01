1b5c0d274b6156d770547b42883fd2a4142e28fb
student777 <darkdakku@gmail.com>
2015-12-01 10:20:15 +0000

    7열 추가, models, admin 변경

--- shelf_map/models.py
+++ shelf_map/models.py
@@ -77,6 +77,19 @@ class Shelf_6(models.Model):
 
     def __str__(self):
         return str(self.major_id) + ' ' + self.minor_id
+        
+        
+class Shelf_7(models.Model):
+    col = models.CharField(max_length=4)
+    row = models.CharField(max_length=4)
+    major_id = models.DecimalField(max_digits=11, decimal_places=7)
+    minor_id = models.CharField(max_length=20)
+
+    class Meta:
+        ordering = ('major_id', 'minor_id', )
+
+    def __str__(self):
+        return str(self.major_id) + ' ' + self.minor_id
 
 
 def create_shelf_1(col, row, major_id, minor_id):
@@ -107,3 +120,8 @@ def create_shelf_5(col, row, major_id, minor_id):
 def create_shelf_6(col, row, major_id, minor_id):
     a = Shelf_6(col=col, row=row, major_id=major_id, minor_id=minor_id)
     a.save()
+
+def create_shelf_7(col, row, major_id, minor_id):
+    a = Shelf_7(col=col, row=row, major_id=major_id, minor_id=minor_id)
+    a.save()
+
