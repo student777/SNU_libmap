18de1ba6e93db800fb132bc27540779c140979b9
memgoon <mem0517@naver.com>
2015-12-01 20:17:24 +0900

    Update read_xls.py
    각 자료실 번호를 하나의 DecimalField로 관리하기 위해 수정합니다.

--- read_xls.py
+++ read_xls.py
@@ -1,4 +1,4 @@
-import xlrd
+import xlrd, pickle
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shelf_map.settings')
 import django
@@ -8,19 +8,25 @@ from django.core.exceptions import ValidationError
 
 
 def main():
-    n = input('몇자료실?')
-    filename = 'static/%s자료실.xlsx' % n
-    workbook = xlrd.open_workbook(filename)
-    workbook_index = workbook.sheet_by_index(0)
-
-    for i in range(0, int(workbook_index.nrows/2)):
-        major_id_list = workbook_index.row_values(2*i)
-        minor_id_list = workbook_index.row_values(2*i+1)
-        for j in range(0, len(major_id_list)):
-            try:
-                shelf_map.models.create_shelf_1(chr(j+65), str(i/2+1), major_id_list[j], minor_id_list[j])
-            except ValidationError:
-                continue
+    #n = input('몇자료실?')
+    f = open("static/자료실.pickle", "rb")
+    room_nums = pickle.load(f)
+    f.close()
+    
+    for room_name in room_nums:
+        filename = room_name
+        workbook = xlrd.open_workbook(filename)
+        workbook_index = workbook.sheet_by_index(0)
+        
+        for i in range(0, int(workbook_index.nrows/2)):
+            major_id_list = workbook_index.row_values(2*i)
+            minor_id_list = workbook_index.row_values(2*i+1)
+            
+            for j in range(0, len(major_id_list)):
+                try:
+                    shelf_map.models.create_shelf(room_nums[roomname], chr(j+65), str(i/2+1), major_id_list[j], minor_id_list[j])
+                except ValidationError:
+                    continue
 
 if __name__ == '__main__':
     main()
