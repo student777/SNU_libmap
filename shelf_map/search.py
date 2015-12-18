27b38213c729f679aa8a3cb4824b1e42ecb25812
student777 <darkdakku@gmail.com>
2015-12-19 05:37:21 +0900

    검색, parsing 개선

--- shelf_map/search.py
+++ shelf_map/search.py
@@ -15,52 +15,49 @@ def search_title(text):
             break
         name = item.a.text
         link = 'http://snu-primo.hosted.exlibrisgroup.com/primo_library/libweb/action/' + item.a['href']
-        if item.find(class_="EXLAvailabilityCollectionName") is None or item.find(class_="EXLAvailabilityCollectionName").text.find('단행본') is -1:
-                continue
+        if item.find(class_="EXLAvailabilityCollectionName") is None or item.find(class_="EXLAvailabilityCollectionName").text.find('단행본자료실') is -1:
+            continue # 규장각, 수원보존도서관, 1-1 및 7-1자료실(?), KOCKA서가 등 제외
         if item.find(class_="EXLResultStatusNotAvailable") is not None:
             status = False
         else:
             status = True
         try:
-            num1 = item.find(class_="EXLAvailabilityCallNumber").text
+            num1 = item.find(class_="EXLAvailabilityCallNumber").text # major_id
         except AttributeError:
             continue
-        num2 = re.search(r"[-+]?\d*\.\d+|\d+", num1)
+        num2 = re.search(r"[a-zA-Z]?\d*\.\d+|[a-zA-Z]?\d+", num1)
         if num2 is None:
             major_id = '확인불가'
             minor_id = ''
             room_num = '확인불가'
             colrow = ''
-        else:
+        elif num1.find('大') is not -1: # ex)大 294 D65b
+            minor_id = num1.split()[2]
+            major_id = '大' + num2.group()
+            room_num = '검색 미지원'
+            colrow = ''
+        elif num2.group()[0].isalpha(): # ex)K781
             major_id = num2.group()
-            try:
-                minor_id = num1.split()[1]
-                if not minor_id[0].isalpha():
-                    minor_id = num1.split()[2]  # 大서가 처리
-            except:
-                minor_id = '확인불가'
-            # TODO: shelf=find_shelf(major_id, minor_id); name, room_num, colrow = shelf.name, shelf.major_id, shelf.minor, shelf.row+shelf.col;
-            shelf_list = Shelf.objects.filter(major_id__lte=major_id).exclude(room_num='1-1').exclude(room_num='7-1').order_by('major_id')
-            if shelf_list.exists():
-                shelf = shelf_list.last()
-                room_num = shelf.room_num + '자료실'
-                colrow = shelf.col + shelf.row
-            else:
-                room_num = '확인불가'
-                colrow = ''
+            minor_id = num1.split()[1]
+            room_num = '검색 미지원'
+            colrow = ''
+        else: # ex) 821.123
+            major_id = num2.group()
+            minor_id = num1.split()[1]
+            shelf = find_shelf(major_id, minor_id)
+            room_num = shelf.room_num + '자료실'
+            colrow = shelf.row+shelf.col
         info_list.append((name, major_id, minor_id, room_num, colrow, link, status))
         i += 1
     return info_list
 
 
-def find_shelf(major_id, minor_id=''):
-    if Shelf.objects.filter(major_id=major_id, major_leadingChr=None).exclude(room_num='1-1').exclude(room_num='7-1').count() > 1:
-        if minor_id is '':
-            result = Shelf.objects.filter(major_id=major_id, major_leadingChr=None).exclude(room_num='1-1').exclude(room_num='7-1').first()
-        else:
-            result = Shelf.objects.filter(major_id=major_id, minor_id__lte=minor_id, major_leadingChr=None).exclude(room_num='7-1').exclude(room_num='1-1').order_by('minor_id').last()
+def find_shelf(major_id, minor_id):
+    shelf_list = Shelf.objects.exclude(room_num='1-1').exclude(room_num='7-1').filter(major_leadingChr=None)
+    if shelf_list.filter(major_id=major_id).count() >= 1:
+        result = shelf_list.filter(major_id=major_id).filter(minor_id__lte=minor_id).last()
+    elif shelf_list.filter(major_id__lte=major_id):
+        result = shelf_list.filter(major_id__lte=major_id).last()
     else:
-        result = Shelf.objects.filter(major_id__lte=major_id, major_leadingChr=None).exclude(room_num='7-1').exclude(room_num='1-1').order_by('major_id').last()
-    if result is None:
         result = Shelf(room_num='검색불가', col='', row='', major_id=0, minor='')
     return result
